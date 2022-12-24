55045f14383704762c37060fc8304d6aad0b9545
Omer Yegen <116505442+Omeryegen@users.noreply.github.com>
2022-12-24 21:29:13 +0300

    Update image.js

--- public/js/image.js
+++ public/js/image.js
@@ -20,7 +20,7 @@ const fetchImage = async ()=>{
     addSpinner();
     button.disabled = true;
     try{    
-        const response = await fetch(rocess.env.CYCLIC_URL, {
+        const response = await fetch(process.env.PORT, {
         method:"POST",
         headers: {
             "Content-Type": "application/json"
